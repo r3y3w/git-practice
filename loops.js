9bb755cf3fadb3e5995e2c767373a6f89e28348f
Rafael Reyes <rafaelreyes@Rafaels-Air.askey.com>
2022-05-30 20:41:07 -0400

    delete tests add loop

---
+++ loops.js
@@ -0,0 +1,24 @@
+// let i = 0 -> initializes varialbe
+// i < 10 -> is the condition to be checked
+// i++ -> this adds 1 to i,  i = i + 1 (++ is the short hand)
+
+// for(let i = 0; i < 10; i ++) {
+//     console.log("loop is running", i)
+//     }
+    
+
+
+
+// for(let i = 0; i <= 100; i ++) {
+//     console.log("Just Testing...", i)
+//     }
+
+const cars = ['chevy', 'volvo', 'saab', 'ford', 'ferrari', 'tesla', 'nissan']
+
+for (let i = 0; i < cars.length; i++) {
+if(i >= 4 && i < 6) {
+    console.log('Check out my car', cars[i])
+    }
+}
+
+
