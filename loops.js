b33ab05144f2cc20929fb5e791ec34884563affb
Rafael Reyes <rafaelreyes@Rafaels-Air.askey.com>
2022-05-30 20:56:55 -0400

    modify loops js file

--- loops.js
+++ loops.js
@@ -13,12 +13,31 @@
 //     console.log("Just Testing...", i)
 //     }
 
-const cars = ['chevy', 'volvo', 'saab', 'ford', 'ferrari', 'tesla', 'nissan']
+// const cars = ['chevy', 'volvo', 'saab', 'ford', 'ferrari', 'tesla', 'nissan']
 
-for (let i = 0; i < cars.length; i++) {
-if(i >= 4 && i < 6) {
-    console.log('Check out my car', cars[i])
-    }
+// for (let i = 0; i < cars.length; i++) {
+// if(i >= 4 && i < 6) {
+//     console.log('Check out my car', cars[i])
+//     }
+// }
+
+
+/**************  LAB   **************
+ * 
+ * // 1. Write a loop that outputs;
+// 10
+// 9
+// 8
+// ...
+// 2
+// 1
+// BlAST OFF!
+ */
+
+for(let i = 10; i >= 0; i --) {
+    if(i > 0) {
+    console.log(i)
+    } else
+    console.log('BLAST OFF !!!!!')
 }
-
 
