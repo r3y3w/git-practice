583eb99ce6b7227f28a97a0219ca230aedde0d20
Rafael Reyes <rafaelreyes@Rafaels-Air.askey.com>
2022-06-02 21:06:12 -0400

    new JS file CalculatorProject

---
+++ CalculatorProject.js
@@ -0,0 +1,57 @@
+/*
+BOCA CODE 
+PTSWE-2022
+Student: Rafael Reyes
+Command Line interface (CLI) Math Calculator
+
+The purpuse of this program, is:
+
+1. Provide an intro for the Math Calculator 
+2. Ask for a User Name
+3. Ask for 2 values to use in a basic math operation (i.e. sum, substract, 
+    multiplcation, division).
+4. Return the result of the math Operation
+5. Ask user if they wan to either run again, or exit
+6. If the selection was to exit, provide an exit statement
+*/
+
+// Intro
+
+console.log("Welcome to Node CLI Calculator App")
+console.log("")
+console.log("Please read the acceptance criteria to get started.")
+// -> Question ???? What is the acceptance criterua?  How should be written?
+
+
+// Ask for user name
+
+const prompt = require('prompt-sync')()
+const userName = prompt('What is your name? --> ')
+console.log(`Hey there ${userName}`)
+
+// Ask what a operation they wat to complete
+
+    // 1. Console.log Which math Operation you would like to do?
+            // 1. for SUM, 2. For Substract 3. for Multiplication  4.Division  5. Exit 
+            
+    // 2. Start a Case Switch 
+            // input option
+            
+// Ask user for first number
+    // store as numVal1 
+
+// Ask user for second number
+    // store as numVal2
+
+
+// Complete operation / display output
+
+    // output result
+
+// Ask user if they want to run again or exit
+
+     // input 1 to run again, any other value to exit
+
+// exit statement
+
+//  thanks username for using the CLI Math Calculator
